8fa8a643ffee46cccf96afb91338ae1c25941fc3
zikit <zikit.start@yandex.ru>
2022-09-28 14:56:57 +0300

    Сделал исправления.

--- src/Author.java
+++ src/Author.java
@@ -1,23 +1,10 @@
 public class Author {
-    private String firstName;
-    private String middleName;
-    private String lastName;
+    private String authorName;
 
-    public Author(String firstName,String middleName,String lastName) {
-        this.firstName = firstName;
-        this.middleName = middleName;
-        this.lastName = lastName;
+    public Author(String firstName , String middleName , String lastName) {
+        this.authorName = firstName + " " + middleName + " " + lastName;
     }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getMiddleName() {
-        return middleName;
-    }
-
-    public String getLastName() {
-        return lastName;
+    public String  getAuthorName() {
+        return authorName;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        Book book1 = new Book("Отцы и дети", 1862);
-        Book book2 = new Book("Мастер и Маргарита",1937);
         Author author1 = new Author("Иван","Сергеевич","Тургенев");
         Author author2 = new Author("Михаил","Афанасьевич","Булгаков");
+
+        Book book1 = new Book("Отцы и дети", 1862, author1);
+        Book book2 = new Book("Мастер и Маргарита", 1937, author2);
+
         book1.setYear(1872);
-        System.out.println(book1.getName() + " - " + author1.getFirstName() + " " + author1.getMiddleName() + " " + author1.getLastName() + " - " + book1.getYear());
-        System.out.println(book2.getName() + " - " + author2.getFirstName() + " " + author2.getMiddleName() + " " + author2.getLastName() + " - " + book2.getYear());
+        System.out.println(book1.getName() + " - " + book1.getAuthor().getAuthorName() + " - " + book1.getYear());
+        System.out.println(book2.getName() + " - " + book2.getAuthor().getAuthorName() + " - " + book2.getYear());
     }
 }
